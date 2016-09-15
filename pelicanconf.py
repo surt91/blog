fafc9634d6b04396d524add046996ea3173a8347
Hendrik Schawe <surt91gmail.com>
2016-09-15 15:14:35 +0200

    new theme with new functionality

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,15 +8,59 @@ SITEURL = ''
 
 TYPOGRIFY = True
 
-THEME = 'themes/pelican-simplegrey'
+#THEME = 'themes/pelican-simplegrey'
+THEME = 'themes/pelican-elegant'
 
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ["render_math"]
+PLUGINS = ["render_math", 
+           "better_code_samples", 
+           "tipue_search",
+           "neighbors",
+           "sitemap"
+]
 MATH_JAX = {"source": "'//blog.schawe.me/mathjax/MathJax.js'"}
 
-STATIC_PATHS = ['extra/favicon.ico']
+STATIC_PATHS = ['extra/favicon.ico', 
+                'extra/jquery.min.js',
+                'extra/bootstrap.min.js',
+                'extra/bootstrap-combined.min.css',
+                'theme/images', 
+                'images',
+                '2009',
+                '2010',
+                '2011',
+                '2012',
+                '2013',
+                '2014',
+]
 EXTRA_PATH_METADATA = {
-    'extra/favicon.ico': {'path': 'favicon.ico'}
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/favicon.ico': {'path': 'theme/images/favicon.ico'},
+    'extra/jquery.min.js': {'path': 'jquery.min.js'},
+    'extra/bootstrap.min.js': {'path': 'bootstrap.min.js'},
+    'extra/bootstrap-combined.min.css': {'path': 'bootstrap-combined.min.css'},
+    'extra/glyphicons-halflings-regular.woff2': {'path': 'glyphicons-halflings-regular.woff2'},
+}
+
+# Elegant theme
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives', 'search', '404'))
+TAG_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+USE_SHORTCUT_ICONS = True
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
 }
 
 PATH = 'content'
@@ -33,10 +77,13 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Ich', 'https://hendrik.schawe.me'),)
+LINKS = (('Ich', 'https://hendrik.schawe.me'),
+         ('Source','https://github.com/surt91/blog'),)
 
 # Social widget
-SOCIAL = (('github', 'http://github.com/surt91'),)
+SOCIAL = (('github', 'https://github.com/surt91'),
+          ('twitter', 'https://twitter.com/surt91'),
+          ('youtube', 'https://www.youtube.com/surt91'))
 
 DEFAULT_PAGINATION = 10
 
--- readme.md
+++ readme.md
@@ -3,10 +3,18 @@ Visit it at [blog.schawe.me](https://blog.schawe.me)
 
 ## dependencies
 1. python3
-2. pelican 3.6.3 (pip3 install pelican)
+
+### from PyPI (pip3 install ...)
+1. pelican 3.6.3 
+1. markdown
+1. typogrify
+1. beautifulsoup4
+1. bs4
+1. html5lib
 
 ## build
 1. clone the repo
 2. `git submodule init; git submodule update`
 3. `make publish`
 4. upload the static files `output` to some webserver
+
