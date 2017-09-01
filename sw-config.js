d98094ea5852374792d51d47a5365f36bfc5555b
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2017-09-01 10:53:52 +0200

    cache svg and larger images, but no videos

--- sw-config.js
+++ sw-config.js
@@ -5,8 +5,10 @@ module.exports = {
     'output/**/**.jpg',
     'output/**/**.png',
     'output/**/**.gif',
+    'output/**/**.svg',
     'output/**/**.webp',
     'output/**/**.js'
   ],
   stripPrefix: 'output',
+  maximumFileSizeToCacheInBytes: 4194304
 }
